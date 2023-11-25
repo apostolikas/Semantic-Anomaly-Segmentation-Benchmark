0031646630a9e6615294b183ef6bfc8c1ed21779
apostolikas <n.apostolikas@hotmail.com>
2023-11-25 22:37:19 +0100

    Train for pvoc and coco

--- Transformaly/train.py
+++ Transformaly/train.py
@@ -2,23 +2,22 @@ import argparse
 import logging
 import pickle
 import os
-from pvoc_data import PascalVOCDataModule
+from data import Coco_Handler, PascalVOCDataModule
 import torch.nn
-from my_utils import *
+from utils import *
 from os.path import join
-from pytorch_pretrained_vit.model import AnomalyViT, ViT, VisionTransformer, vit_base, vit_small
+from pytorch_pretrained_vit.model import AnomalyViT, ViT
+from timm.models.vision_transformer import vit_base_patch16_224
+import numpy as np
 import torchvision.transforms as transforms
-from functools import partial
-from timm.models.vision_transformer import vit_small_patch16_224, vit_base_patch16_224
-
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--dataset',default='cifar100')
-    parser.add_argument('--data_path', default='/var/scratch/napostol/standard_data/', help='Path to the dataset')
-    parser.add_argument('--epochs', type=int, default=1, help='Number of epochs')
-    parser.add_argument('--batch_size', type=int, default=6, help='Training batch size')
+    parser = argparse.ArgumentParser(description='Transformaly')
+    parser.add_argument('--dataset',default='pvoc')
+    parser.add_argument('--data_path', default='./Transformaly_datasets/', help='Path to the dataset')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
+    parser.add_argument('--batch_size', type=int, default=16, help='Training batch size')
     parser.add_argument('--lr', default=0.0001,
                         help='Learning rate value')
     parser.add_argument('--eval_every', type=int, default=2,
@@ -27,12 +26,13 @@ if __name__ == '__main__':
                         help='Use the unimodal settings')
     parser.add_argument('--plot_every_layer_summarization', default=False, action='store_true',
                         help='plot the per layer AUROC')
+    parser.add_argument('--mode', default='trained', choices=['trained', 'pretrained'], help='Use the trained or pretrained model')
     parser_args = parser.parse_args()
     args = vars(parser_args)
 
 
     args['use_layer_outputs'] = list(range(2, 12))
-    args['use_imagenet'] = False # Was True
+    args['use_imagenet'] = False
     BASE_PATH = 'experiments'
 
     if args['dataset'] == 'cifar10':
@@ -47,8 +47,9 @@ if __name__ == '__main__':
         _classes = range(19)
     elif args['dataset'] == 'pvoc':
         _classes = range(20)
-    elif args['dataset'] == 'mnist':
-        _classes = range(10)
+    elif args['dataset'] == 'coco':
+        _classes = range(92)
+        _classes = [i for i in _classes if i not in [0, 12, 26, 29, 30, 45, 66, 68, 69, 71, 83, 91]]
     else:
         raise ValueError(f"Does not support the {args['dataset']} dataset")
     # create the relevant directories
@@ -68,190 +69,182 @@ if __name__ == '__main__':
     print_and_add_to_log(args, logging)
     print_and_add_to_log("========================================================", logging)
 
-    
-    # for _class in _classes:
-    #     print_and_add_to_log("===================================", logging)
-    #     print_and_add_to_log(f"Class is : {_class}", logging)
-    #     print_and_add_to_log("===================================", logging)
-    #     args['_class'] = _class
-    #     base_feature_path = join(
-    #         BASE_PATH,
-    #         f'{"unimodal" if args["unimodal"] else "multimodal"}/{args["dataset"]}/class_{_class}')
-    #     model_path = join(base_feature_path, 'model')
+    for _class in _classes:
+        print_and_add_to_log("===================================", logging)
+        print_and_add_to_log(f"Class is : {_class}", logging)
+        print_and_add_to_log("===================================", logging)
+        args['_class'] = _class
+        base_feature_path = join(
+            BASE_PATH,
+            f'{"unimodal" if args["unimodal"] else "multimodal"}/{args["dataset"]}/class_{_class}')
+        model_path = join(base_feature_path, 'model')
 
-    #     args['base_feature_path'] = base_feature_path
-    #     args['model_path'] = model_path
+        args['base_feature_path'] = base_feature_path
+        args['model_path'] = model_path
 
-    #     # create the relevant directories
-    #     if not os.path.exists(model_path):
-    #         os.makedirs(model_path)
+        # create the relevant directories
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
 
-    #     if args['unimodal']:
-    #         anomaly_classes = [i for i in _classes if i != args['_class']]
-    #     else:
-    #         anomaly_classes = [args['_class']]
+        if args['unimodal']:
+            anomaly_classes = [i for i in _classes if i != args['_class']]
+        else:
+            anomaly_classes = [args['_class']]
 
-    #     print_and_add_to_log(
-    #         "====================================================================",
-    #         logging)
-    #     print_and_add_to_log(
-    #         "Start Training", logging)
-    #     print_and_add_to_log(
-    #         "====================================================================",
-    #         logging)
+        print_and_add_to_log(
+            "====================================================================",
+            logging)
+        print_and_add_to_log(
+            "Start Training", logging)
+        print_and_add_to_log(
+            "====================================================================",
+            logging)
 
-        # trainset, testset = get_datasets_for_ViT(dataset=args['dataset'],
-        #                                          data_path=args['data_path'],
-        #                                          one_vs_rest=args['unimodal'],
-        #                                          _class=args['_class'],
-        #                                          normal_test_sample_only=True,
-        #                                          use_imagenet=args['use_imagenet']
-        #                                          )
-        # # trainset testset me ola xwris to _class
-        # _, ood_test_set = get_datasets_for_ViT(dataset=args['dataset'],
-        #                                        data_path=args['data_path'],
-        #                                        one_vs_rest=not args['unimodal'],
-        #                                        _class=args['_class'],
-        #                                        normal_test_sample_only=True,
-        #                                        use_imagenet=args['use_imagenet']
-        #                                        )
-        # # ood mono me to _class
-        # print_and_add_to_log("---------------", logging)
-        # print_and_add_to_log(f'Class size: {args["_class"]}', logging)
-        # print_and_add_to_log(f'Trainset size: {len(trainset)}', logging)
-        # print_and_add_to_log(f'Testset size: {len(testset)}', logging)
-        # print_and_add_to_log(f'OOD testset size: {len(ood_test_set)}', logging)
-        # print_and_add_to_log("---------------", logging)
-        # train_loader = torch.utils.data.DataLoader(trainset, batch_size=args['batch_size'],
-        #                                            shuffle=True)
-        # val_loader = torch.utils.data.DataLoader(testset, batch_size=args['batch_size'],
-        #                                          shuffle=False)
-        # ood_val_loader = torch.utils.data.DataLoader(ood_test_set, batch_size=args['batch_size'],
-        #                                              shuffle=False)
+        if args['dataset'] == 'coco':
+        
+            mod = Coco_Handler(
+                batch_size=args['batch_size'], 
+                normal_classes=[args['_class']], 
+                transformations=transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()]),
+                num_workers=2
+                )
+            testset = mod.test_dataset
+            dataloaders = {'training': mod.get_train_loader(),
+                        'val': mod.get_test_loader_normal(),
+                        'test': mod.get_test_loader_ood(),
+                        'overall_test': mod.get_test_loader(),
+                        }   
 
+        elif args['dataset'] == 'pvoc':
+         
+            mod = PascalVOCDataModule(
+                batch_size=args['batch_size'], 
+                normal_classes=[args['_class']], 
+                image_train_transform=transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()])
+                )
+            testset = mod.test_dataset
+            dataloaders = {'training': mod.get_train_dataloader(),
+                        'val': mod.get_normal_test_dataloader(),
+                        'test': mod.get_ood_test_dataloader(),
+                        'overall_test': mod.get_test_dataloader(),
+                        }
 
-        # train_transform= transforms.Compose([
-        #             transforms.Resize((384, 384)),
-        #             transforms.ToTensor(),
-        #     ])
-        # normal_classes = [_class]
-        # mod = PascalVOCDataModule(batch_size=8, train_transform=train_transform, val_transform=train_transform, test_transform=train_transform, normal_classes = normal_classes)
-        # val_loader = mod.get_test_dataloader()
+        else:
 
-        # _normal_classes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-        # _normal_classes.pop(_class)
-        # mod1 = PascalVOCDataModule(batch_size=8, train_transform=train_transform, val_transform=train_transform, test_transform=train_transform, normal_classes = _normal_classes)
-        # train_loader = mod1.get_train_dataloader()
-        # ood_val_loader = mod1.get_test_dataloader()
-        # print(f"Train set size : {len(train_loader)}")
-        # print(f"Test set size : {len(val_loader)}")
-        # print(f"OOD set size : {len(ood_val_loader)}")
-        # dataloaders = {'training': train_loader,
-        #                'val': val_loader,
-        #                'test': ood_val_loader
-        #                }
+            trainset, testset = get_datasets_for_ViT(dataset=args['dataset'], data_path=args['data_path'], one_vs_rest=args['unimodal'], _class=args['_class'], normal_test_sample_only=True, use_imagenet=args['use_imagenet'])
+            _, ood_test_set = get_datasets_for_ViT(dataset=args['dataset'], data_path=args['data_path'], one_vs_rest=not args['unimodal'], _class=args['_class'], normal_test_sample_only=True, use_imagenet=args['use_imagenet'])
 
-        # Build model
-        # if args['use_imagenet']:
-        #     VIT_MODEL_NAME = 'B_16_imagenet1k'
-        # else:
-        #     VIT_MODEL_NAME = 'B_16'
+            print_and_add_to_log("---------------", logging)
+            print_and_add_to_log(f'Class size: {args["_class"]}', logging)
+            print_and_add_to_log(f'Trainset size: {len(trainset)}', logging)
+            print_and_add_to_log(f'Testset size: {len(testset)}', logging)
+            print_and_add_to_log(f'OOD testset size: {len(ood_test_set)}', logging)
+            print_and_add_to_log("---------------", logging)
+
+            train_loader = torch.utils.data.DataLoader(trainset, batch_size=args['batch_size'],
+                                                    shuffle=True)
+            val_loader = torch.utils.data.DataLoader(testset, batch_size=args['batch_size'],
+                                                    shuffle=False)
+            ood_val_loader = torch.utils.data.DataLoader(ood_test_set, batch_size=args['batch_size'],
+                                                        shuffle=False)
+            dataloaders = {'training': train_loader,
+                        'val': val_loader,
+                        'test': ood_val_loader
+                        }
 
         # Build model
-        # model = AnomalyViT(VIT_MODEL_NAME, pretrained=True)
-        # model.fc = Identity()
-        # # Build model for best instance
-        # best_model = AnomalyViT(VIT_MODEL_NAME, pretrained=True)
-        # best_model.fc = Identity()
+        if args['use_imagenet']:
+            VIT_MODEL_NAME = 'B_16_imagenet1k'
+        else:
+            VIT_MODEL_NAME = 'B_16'
 
-        # model.to('cuda')
-        # best_model.to('cuda')
+        # Build model
+        model = AnomalyViT(VIT_MODEL_NAME, pretrained=True)
+        model.fc = Identity()
+        # Build model for best instance
+        best_model = AnomalyViT(VIT_MODEL_NAME, pretrained=True)
+        best_model.fc = Identity()
 
-        # model_checkpoint_path = join(model_path, 'last_full_finetuned_model_state_dict.pkl')
-        # if os.path.exists(model_checkpoint_path):
-        #     model_state_dict = torch.load(model_checkpoint_path)
-        #     model.load_state_dict(model_state_dict)
-        #     print_and_add_to_log("model loadded from checkpoint here:", logging)
-        #     print_and_add_to_log(model_checkpoint_path, logging)
+        model.to('cuda')
+        best_model.to('cuda')
 
-
-        # freeze the model
-        # freeze_finetuned_model(model)
-        # model, best_model, cur_acc_loss = train(model=model,
-        #                                         best_model=best_model,
-        #                                         args=args,
-        #                                         dataloaders=dataloaders,
-        #                                         output_path=model_path,
-        #                                         device='cuda',
-        #                                         seed=42,
-        #                                         model_checkpoint_path=None,
-        #                                         #model_checkpoint_path=model_checkpoint_path,
-        #                                         anomaly_classes=anomaly_classes
-        #                                         )
-
-        # training_losses = cur_acc_loss['training_losses']
-        # val_losses = cur_acc_loss['val_losses']
+        model_checkpoint_path = join(model_path, 'last_full_finetuned_model_state_dict.pkl')
+        if os.path.exists(model_checkpoint_path):
+            model_state_dict = torch.load(model_checkpoint_path)
+            model.load_state_dict(model_state_dict)
+            print_and_add_to_log("model loadded from checkpoint here:", logging)
+            print_and_add_to_log(model_checkpoint_path, logging)
 
 
+        if args['mode'] == 'trained':
 
+            freeze_finetuned_model(model)
+            model, best_model, cur_acc_loss = train(model=model,
+                                                    best_model=best_model,
+                                                    args=args,
+                                                    dataloaders=dataloaders,
+                                                    output_path=model_path,
+                                                    device='cuda',
+                                                    seed=42,
+                                                    model_checkpoint_path=model_checkpoint_path,
+                                                    anomaly_classes=anomaly_classes
+                                                    )
 
-        # try:
-        #     plot_graphs(training_losses, val_losses, training_losses, val_losses)
+            training_losses = cur_acc_loss['training_losses']
+            val_losses = cur_acc_loss['val_losses']
+            try:
+                plot_graphs(training_losses, val_losses, training_losses, val_losses)
 
-        # except Exception as e:
-        #     print_and_add_to_log('raise error:', logging)
-        #     print_and_add_to_log(e, logging)
+            except Exception as e:
+                print_and_add_to_log('raise error:', logging)
+                print_and_add_to_log(e, logging)
 
-        # save models
-        # torch.save(best_model.state_dict(), join(model_path,
-        #                                          'best_full_finetuned_model_state_dict.pkl'))
-        # torch.save(model.state_dict(), join(model_path,
-        #                                     'last_full_finetuned_model_state_dict.pkl'))
+            # save models
+            torch.save(best_model.state_dict(), join(model_path,
+                                                    'best_full_finetuned_model_state_dict.pkl'))
+            torch.save(model.state_dict(), join(model_path,
+                                                'last_full_finetuned_model_state_dict.pkl'))
 
-        # save losses
-        # with open(join(model_path, 'full_finetuned_training_losses.pkl'), 'wb') as f:
-        #     pickle.dump(training_losses, f)
-        # with open(join(model_path, 'full_finetuned_val_losses.pkl'), 'wb') as f:
-        #     pickle.dump(val_losses, f)
+            # save losses
+            with open(join(model_path, 'full_finetuned_training_losses.pkl'), 'wb') as f:
+                pickle.dump(training_losses, f)
+            with open(join(model_path, 'full_finetuned_val_losses.pkl'), 'wb') as f:
+                pickle.dump(val_losses, f)
 
-    # if args['use_imagenet']:
-    #     MODEL_NAME = 'B_16_imagenet1k'
-    # else:
-    #     MODEL_NAME = 'B_16'
+            if args['use_imagenet']:
+                MODEL_NAME = 'B_16_imagenet1k'
+            else:
+                MODEL_NAME = 'B_16'
 
-    # Use the pretrained model
+            model = ViT(MODEL_NAME, pretrained=True)
+            model.fc = Identity()
+            model.load_state_dict(best_model.state_dict(), strict=False)
 
-    #model = ViT('B_16', pretrained=True)
+        elif args['mode'] == 'pretrained':
 
-    # pretraining = vit_small_patch16_224(pretrained=True).cuda()
-    # model = vit_small(pretrained=True)
+            if args['use_imagenet']:
+                MODEL_NAME = 'B_16_imagenet1k'
+            else:
+                MODEL_NAME = 'B_16'
 
-    pretraining = vit_base_patch16_224(pretrained=True).cuda()
-    model = vit_base(pretraining=True)
-    
-    model.load_state_dict(pretraining.state_dict(), strict=False)
-    model.fc = Identity()
-    model = model.cuda()
-    model.eval()
+            model = ViT(MODEL_NAME, pretrained=True)
+            model.fc = Identity()
+            pretraining = vit_base_patch16_224(pretrained=True).to('cuda')
+            model.load_state_dict(pretraining.state_dict(), strict=False)
 
-    # model = torch.hub.load('facebookresearch/dino:main', 'dino_vits16')
-    # model = torch.hub.load('facebookresearch/dino:main', 'dino_vitb16')
-    # model.fc = Identity()
-    # model = model.cuda()
-    # model.eval()
+        else:
+            raise ValueError(f"Does not support the {args['mode']} mode")
 
-    auc = extract_features(base_path=BASE_PATH,
-                    data_path=args['data_path'],
-                    datasets=[args['dataset']],
-                    #datasets = ['VOCDataset'],
-                    model=model,
-                    logging=logging,
-                    calculate_features=True,
-                    unimodal_vals=[args['unimodal']],
-                    manual_class_num_range=ModuleNotFoundError,
-                    output_train_features=True,
-                    output_test_features=True,
-                    use_imagenet=args['use_imagenet'])
-    
-    print(auc)
-    
+        model.eval()
+        model.to('cuda')
+        
+        extract_features(base_path=BASE_PATH,
+                        data_path=args['data_path'],
+                        datasets=[args['dataset']],
+                        model=model,
+                        logging=logging,
+                        calculate_features=True,
+                        unimodal_vals=[args['unimodal']],
+                        output_train_features=True,
+                        output_test_features=True,
+                        use_imagenet=args['use_imagenet'])
